97563ea9078b0ced246ad2a0593f15dabd871ae5
Sudheer <sudheer.gadhala@kagamierp.com>
2023-03-17 17:36:29 +0530

    sfwfe

--- src/D3Curve.tsx
+++ src/D3Curve.tsx
@@ -1,6 +1,46 @@
 import React, { useRef, useEffect } from 'react';
 import * as d3 from 'd3';
 import { empData as data } from "./assets/TestData";
+import "./assets/styles.css";
+
+function getProbabilityData(normalizedData, m, v) {
+  var data = [];
+  for (var i = 0; i < normalizedData.length; i += 1) {
+    var q = normalizedData[i].rating,
+      q1 = q,//(q - m) / v,
+      p = probabilityDensityCalculation(q1, m, v),
+      el = {
+        x: q,
+        y: p,
+        z: q1,
+        c: normalizedData[i].category,
+        e: normalizedData[i].empId
+      };
+    data.push(el);
+  };
+  data.sort(function (x, y) { return x.x - y.x; });
+  return data;
+};
+
+function probabilityDensityCalculation(x, mean, variance) {
+  var m = Math.sqrt(2 * Math.PI * variance);
+  var e = Math.exp(-Math.pow(x - mean, 2) / (2 * variance));
+  return e / m;
+};
+
+function kernelDensityEstimator(kernel, X) {
+  return function (V) {
+    return X.map(function (x) {
+      return [x, d3.mean(V, function (v: any) { return kernel(x - v); })];
+    });
+  };
+}
+
+function kernelEpanechnikov(k) {
+  return function (v) {
+    return Math.abs(v /= k) <= 1 ? 0.75 * (1 - v * v) / k : 0;
+  };
+}
 
 function D3Curve() {
   const ref = useRef<SVGSVGElement | null>(null);
@@ -17,55 +57,122 @@ function D3Curve() {
     const chartWidth = width - margin.left - margin.right;
     const chartHeight = height - margin.top - margin.bottom;
 
+    const colors = ["#be280c", "#c0a92c", "#105526", "#034aea", "#1502cf"];
+
+    var svg = d3.select(ref.current);
+    svg.selectAll("*").remove();
+
+    svg.attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
     const mean = d3.mean(data.map(d => d.rating));
     const deviation = d3.deviation(data.map(d => d.rating)) as number;
+    const normalizedData = getProbabilityData(data, mean, deviation);
 
-    const xScale = d3
-      .scaleLinear()
-      .domain([d3.min(data)!, d3.max(data)!])
-      .range([margin.left, width - margin.right]);
+    var x = d3.scaleLinear()
+      .range([0, chartWidth]);
 
-    const yScale = d3
-      .scaleLinear()
-      .domain([
-        0,
-        d3.max(
-          data.map((d) =>
-            d3
-              .normal(mean, deviation)
-              .pdf(d) as number
-          )
-        )!,
-      ])
-      .range([chartHeight, margin.top]);
+    var y = d3.scaleLinear()
+      .range([(chartHeight * (2 / 3)), 6]);
 
-    // Create the curve generator
-    const curveGenerator = d3
-      .line()
-      .curve(d3.curveBasis)
-      .x((d) => xScale(d))
-      .y((d) => yScale(d3.normal(mean, deviation).pdf(d) as number));
 
-    // Create the bell curve path
-    const pathSelection = d3.select(ref.current).append('path');
+    const group1 = normalizedData.filter(d => d.x <= 1);
+    const group2 = normalizedData.filter(d => d.x > 1 && d.x <= 2);
+    const group3 = normalizedData.filter(d => d.x > 2 && d.x <= 3);
+    const group4 = normalizedData.filter(d => d.x > 3 && d.x <= 4);
+    const group5 = normalizedData.filter(d => d.x > 4);
 
-    pathSelection
-      .attr('d', curveGenerator(d3.range(d3.min(data)!, d3.max(data)!, 0.1)))
-      .attr('fill', 'none')
-      .attr('stroke', '#69b3a2');
+    var line = d3.line<{ x: number, y: number, z: number }>()
+      .x(function (d) { return x(d.z); })
+      .y(function (d) { return y(d.y); });
 
-    // Add the shaded zones to the bell curve
-    const zonesSelection = d3.select(ref.current).selectAll('rect').data([1, 2, 3]).enter().append('rect');
+    x.domain(d3.extent(normalizedData, function (d) {
+      return d.z;
+    }));
+    y.domain(d3.extent(normalizedData, function (d) {
+      return d.y;
+    }));
 
-    zonesSelection
-      .attr('x', (d) => xScale(mean - d * deviation))
-      .attr('width', (d) => (d === 1 ? xScale(mean + d * deviation) : xScale(mean + d * deviation) - xScale(mean - d * deviation)))
-      .attr('y', margin.top)
-      .attr('height', chartHeight - margin.top - margin.bottom)
-      .attr('fill', (d) => (d === 1 ? 'rgba(105, 179, 162, 0.2)' : 'rgba(105, 179, 162, 0.4)'));
+    // svg.append("path")
+    //   .datum(normalizedData)
+    //   .attr("class", "line")
+    //   .attr("d", line)
+    //   .attr("fill", "none")
+    //   .attr("stroke", "steelblue")
+    //   .attr("stroke-width", 2);
+
+   const density = kernelDensityEstimator(kernelEpanechnikov(7), x.ticks(40))(normalizedData.map(d=>d.y));
+
+    svg
+      .append('g')
+      .append("path")
+      .attr("class", "group group1")
+      .datum(density)
+      .attr("fill", colors[0])
+      .attr("d", d3.area()
+        .curve(d3.curveLinear)
+        .x(d=>x(d[1]))
+        .y0(y(0))
+        .y1(d=> y(d[0]))
+      );
+
+    svg
+      .append('g')
+      .append("path")
+      .attr("class", "group group2")
+      .datum(group2)
+      .attr("fill", colors[1])
+      .attr("d", d3.area()
+        .curve(d3.curveLinear)
+        .x(function (d: any) { return x(d.z); })
+        .y0(y(0))
+        .y1(function (d: any) { return y(d.y); })
+      );
+
+    svg
+      .append('g')
+      .append("path")
+      .attr("class", "group group3")
+      .datum(group3)
+      .attr("fill", colors[2])
+      .attr("d", d3.area()
+        .curve(d3.curveLinear)
+        .x(function (d: any) { return x(d.z); })
+        .y0(y(0))
+        .y1(function (d: any) { return y(d.y); })
+      );
+
+    svg
+      .append('g')
+      .append("path")
+      .attr("class", "group group4")
+      .datum(group4)
+      .attr("fill", colors[3])
+      .attr("d", d3.area()
+        .curve(d3.curveLinear)
+        .x(function (d: any) { return x(d.z); })
+        .y0(y(0))
+        .y1(function (d: any) { return y(d.y); })
+      );
+
+    svg
+      .append('g')
+      .append("path")
+      .attr("class", "group group5")
+      .datum(group5)
+      .attr("fill", colors[4])
+      .attr("d", d3.area()
+        .curve(d3.curveLinear)
+        .x(function (d: any) { return x(d.z); })
+        .y0(y(0))
+        .y1(function (d: any) { return y(d.y); })
+      );
+
   }, [data]);
 
-  return <svg ref={ref} width={800} height={400}></svg>;
+  return <svg viewBox='0 0 860 450' ref={ref}></svg>;
 }
 
 export default D3Curve
